4a6b48a6b4cd38020e1d227a0a95073e334dd328
Jeff Jakinovich <53125676+jljakin2@users.noreply.github.com>
2024-08-07 23:02:13 -0400

    added the needed pieces from shipfast repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      // NextJS <Image> component needs to whitelist domains for src={}
+      "",
+    ],
+  },
+};
 
 export default nextConfig;
